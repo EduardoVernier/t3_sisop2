2082c73fa5efca1ac2e1b119602e178127828a5a
Eduardo Vernier <efvernier@gmail.com>
2015-05-21 16:05:54 -0300

    Server accepting multiple connections

--- server.c
+++ server.c
@@ -9,19 +9,59 @@
 
 #define MAX_CONNECTIONS 10
 
-void startConnection (int sockfd);
+int sockfd, newsockfd;
+pthread_t runner;
+
+void *newClientConnection (void *arg);
+int setUpSocket(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-	int sockfd, newsockfd;
+	setUpSocket(argc, argv);
+	struct sockaddr_in cli_addr;
+	
+	
 	socklen_t clilen;
-	struct sockaddr_in serv_addr, cli_addr;
+	clilen = sizeof(struct sockaddr_in);
+	while (newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen))
+	{
+		puts("Connection accepted");
+         
+        pthread_t newClientThread;
+        int *newSock = malloc(sizeof(int));
+        *newSock = newsockfd;
+		
+		if( pthread_create( &newClientThread , NULL ,  newClientConnection , (void*)newSock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+        
+			//startConnection (); //new thread for each conection
+
+
+	}
+	
+	/* read from the socket */
+	/* write in the socket */ 
+	//n = write(newsockfd,"I got your message", 18);
+	//if (n < 0) 
+	//	printf("ERROR writing to socket");
+
+	close(newsockfd);
+	close(sockfd);
+	return 0; 
+}
+
+int setUpSocket(int argc, char *argv[])
+{
+	struct sockaddr_in serv_addr;
 	int port;	
 
 	if (argc < 2)
 	{
 		fprintf(stderr,"usage %s #port\n", argv[0]);
-		return(0);
+		return(1);
 	}
 	
 	port = atoi(argv[1]);
@@ -34,40 +74,18 @@ int main(int argc, char *argv[])
 	bzero(&(serv_addr.sin_zero), 8);     
     
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-		printf("ERROR on binding");
-	
-	listen(sockfd, MAX_CONNECTIONS);
-	
-	int on = 1; // debugging purpose
-	while (on)
 	{
-		printf("enter loop\n");
-		clilen = sizeof(struct sockaddr_in);
-		if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) == -1) 
-			printf("ERROR on accept");
-		else
-			startConnection (newsockfd); //new thread for each conection
-		printf("loop\n");
-
+		printf("ERROR on binding");
+		return 1;	
 	}
-	
-	/* read from the socket */
-	
-	
-	
-	/* write in the socket */ 
-	//n = write(newsockfd,"I got your message", 18);
-	//if (n < 0) 
-	//	printf("ERROR writing to socket");
-
-	close(newsockfd);
-	close(sockfd);
-	return 0; 
+		
+	listen(sockfd, MAX_CONNECTIONS);
 }
 
-void startConnection (int newsockfd)
+void *newClientConnection (void *arg)
 {
-
+	int newsockfd = *(int*) arg;
+	
 	char buffer[256];
 	bzero(buffer, 256);
 	printf("New client!\n");
@@ -81,6 +99,4 @@ void startConnection (int newsockfd)
 	}
 	if (write(newsockfd, "/disconnect\n", 12) < 0)
 		printf("Error disconnecting client");
-
-		
 }
