8ce9c612b6d284ef773459b2526a9fe5c02dc040
IagoOne <iago_one@ymail.com>
2015-05-24 06:50:33 -0300

    -- bug de impressão de mensagens antigas resolvido, agora imprime a mensagem recebida corretamente

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <pthread.h>
 
 int sockfd;
@@ -20,13 +20,13 @@ int main(int argc, char *argv[])
 {
 	if (connectToServer(argc, argv) == 1)
 		return 0;
-	
+
 	pthread_create(&sender, NULL, sendMessages, NULL);
 	pthread_create(&receiver, NULL, receiveMessages, NULL);
-	
+
 	pthread_join(sender, NULL);
 	pthread_join(receiver, NULL);
-	
+
 	close(sockfd);
 	return 0;
 }
@@ -36,11 +36,11 @@ int connectToServer(int argc, char *argv[])
 	if (argc < 3) {
 		fprintf(stderr,"usage %s hostname #port\n", argv[0]);
 		return(1);
-	}	
+	}
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 	int port;
-	
+
 	port = atoi(argv[2]);
 	server = gethostbyname(argv[1]);
 	if (server == NULL) {
@@ -48,18 +48,18 @@ int connectToServer(int argc, char *argv[])
 		return(1);
 	}
 
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		printf("ERROR opening socket\n");
 		return 1;
 	}
-	
-	serv_addr.sin_family = AF_INET;     
-	serv_addr.sin_port = htons(port);    
-	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
-	bzero(&(serv_addr.sin_zero), 8);     
 
-	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
+	bzero(&(serv_addr.sin_zero), 8);
+
+	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
 	{
 		printf("ERROR connecting\n");
 		return 1;
@@ -70,7 +70,7 @@ int connectToServer(int argc, char *argv[])
  {
  	char buffer[256];
  	while (strcmp(buffer, "/logout\n") != 0)
-	{	
+	{
 		printf("Enter the message: ");
 		bzero(buffer, 256);
 		fgets(buffer, 256, stdin);
@@ -78,11 +78,12 @@ int connectToServer(int argc, char *argv[])
 		/* write in the socket */
 		int n;
 		n = write(sockfd, buffer, strlen(buffer));
-		if (n < 0) 
-			printf("ERROR writing to socket\n");	
+		if (n < 0)
+			printf("ERROR writing to socket\n");
+        bzero(buffer, 256);
 	}
  }
- 
+
  void *receiveMessages(void *arg)
  {
  	char buffer[256];
@@ -91,10 +92,10 @@ int connectToServer(int argc, char *argv[])
 	{
 		bzero(buffer,256);
 		n = read(sockfd, buffer, 256);
-		if (n < 0) 
+		if (n < 0)
 			printf("ERROR reading from socket\n");
 		printf("%s\n", buffer);
  	}
  }
- 
- 
+
+
--- server.c
+++ server.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include "message_list.c"
 
 #define MAX_CONNECTIONS 10
@@ -13,6 +14,7 @@
 int sockfd, newsockfd;
 pthread_t runner;
 message_list* messageList;
+sem_t semaphore;
 
 void *newClientConnection (void *arg);
 int setUpSocket(int argc, char *argv[]);
@@ -21,71 +23,73 @@ int main(int argc, char *argv[])
 {
 	setUpSocket(argc, argv);
 	struct sockaddr_in cli_addr;
-	
+
 	messageList = newMessageList();
+
+    sem_init(&semaphore, 0, 1);
 
 	socklen_t clilen;
 	clilen = sizeof(struct sockaddr_in);
 	while (newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen))
 	{
 		puts("Connection accepted");
-         
+
         pthread_t newClientThread;
         int *newSock = malloc(sizeof(int));
         *newSock = newsockfd;
-		
+
 		if( pthread_create( &newClientThread , NULL ,  newClientConnection , (void*)newSock) < 0)
         {
             perror("could not create thread");
             return 1;
         }
-        
+
 	}
-	
+
 	/* read from the socket */
-	/* write in the socket */ 
+	/* write in the socket */
 	//n = write(newsockfd,"I got your message", 18);
-	//if (n < 0) 
+	//if (n < 0)
 	//	printf("ERROR writing to socket");
 
 	close(newsockfd);
 	close(sockfd);
-	return 0; 
+	return 0;
 }
 
 int setUpSocket(int argc, char *argv[])
 {
 	struct sockaddr_in serv_addr;
-	int port;	
+	int port;
 
 	if (argc < 2)
 	{
 		fprintf(stderr,"usage %s #port\n", argv[0]);
 		return(1);
 	}
-	
+
 	port = atoi(argv[1]);
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         printf("ERROR opening socket");
-	
+
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(serv_addr.sin_zero), 8);     
-    
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+	bzero(&(serv_addr.sin_zero), 8);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 	{
 		printf("ERROR on binding");
 		return 1;
 	}
-		
+
 	listen(sockfd, MAX_CONNECTIONS);
 }
 
 void *newClientConnection (void *arg)
 {
 	int newsockfd = *(int*) arg;
-	
+
 	char buffer[256];
 	bzero(buffer, 256);
 	printf("New client!\n");
@@ -93,15 +97,21 @@ void *newClientConnection (void *arg)
 	{
 		int n;
 		n = read(newsockfd, buffer, 256);
-		if (n < 0) 
+		if(n < 0)
 			printf("ERROR reading from socket");
+
+        sem_wait(&semaphore);
+
 		printf("Here is the message: %s", buffer);
-		
+		bzero(buffer, 256);
+
 		// provisory - room will be sent with every message (parsing needed)
 		char _room [10] = "futebol";
 		message* newM = newMessage(buffer, _room);
 		addNewMessage(messageList, newM);
-		printMessages(messageList);
+		//printMessages(messageList);
+
+		sem_post(&semaphore);
 	}
 	if (write(newsockfd, "/disconnect\n", 12) < 0)
 		printf("Error disconnecting client");
